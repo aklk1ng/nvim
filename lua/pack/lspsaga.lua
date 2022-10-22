98c9044007bf804b5ae66fcc79186467ad70c86a
aklk1ng <2628084745@qq.com>
2022-10-22 20:47:22 +0800

    update lspsaga

--- lua/pack/lspsaga.lua
+++ lua/pack/lspsaga.lua
@@ -37,7 +37,7 @@ saga.init_lsp_saga {
     code_action_num_shortcut = true,
     -- same as nvim-lightbulb but async
     code_action_lightbulb = {
-        enable = true,
+        enable = false,
         enable_in_insert = true,
         cache_code_action = false,
         sign = false,
