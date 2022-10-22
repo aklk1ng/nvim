9317ffed93213c9802c8859f1fc50728b7e03396
aklk1ng <2628084745@qq.com>
2022-10-22 18:42:13 +0800

    add macos and windows config

--- init.lua
+++ init.lua
@@ -2,3 +2,14 @@ require('profile')
 require('mapping')
 require('packer-init')
 require('pack.lsp.setup')
+
+local has = vim.fn.has
+local is_mac = has "macunix"
+local is_win = has "win32"
+
+if is_mac then
+  require('macos')
+end
+if is_win then
+  require('windows')
+end
