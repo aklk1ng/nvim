7b8472d19b615c7b0f307affaa9c925613f8f45f
aklk1ng <2628084745@qq.com>
2022-10-22 16:35:42 +0800

    update snippets

--- lua/pack/cmp.lua
+++ lua/pack/cmp.lua
@@ -6,6 +6,7 @@ local has_words_before = function()
     return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
 end
 require("luasnip.loaders.from_vscode").lazy_load()
+require("luasnip.loaders.from_vscode").lazy_load({path = "../snippets"})
 cmp.setup({
     snippet = {
         expand = function(args)
