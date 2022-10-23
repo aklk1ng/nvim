a50d80b8728c04ca8756b1dcafe3ab683564d8d9
aklk1ng <2628084745@qq.com>
2022-10-23 18:30:21 +0800

    update makrdown

--- lua/pack/markdown.lua
+++ lua/pack/markdown.lua
@@ -2,7 +2,7 @@ local G = require('G')
 local M = {}
 
 function M.config()
-    G.g.mkdp_markdown_css = '/home/cjh/.config/nvim/colors/markdown.css'
+    G.g.mkdp_markdown_css = '~/.config/nvim/colors/markdown.css'
     G.g.mkdp_page_title = '${name}'
     G.g.mkdp_preview_options = { hide_yaml_meta = 1, disable_filename = 1 }
 end
