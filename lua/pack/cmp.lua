a965b4c36b9793848e0a1689aa08993e843e69b7
aklk1ng <2628084745@qq.com>
2022-10-23 19:03:36 +0800

    change the completion map and cooperate the snippets

--- lua/pack/cmp.lua
+++ lua/pack/cmp.lua
@@ -1,12 +1,7 @@
 local cmp = require'cmp'
 local lspkind = require'lspkind'
 local luasnip = require'luasnip'
-local has_words_before = function()
-    local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-    return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
-end
 require("luasnip.loaders.from_vscode").lazy_load()
-require("luasnip.loaders.from_vscode").lazy_load({path = "../snippets"})
 cmp.setup({
     snippet = {
         expand = function(args)
@@ -32,21 +27,16 @@ cmp.setup({
             select = true,
         }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
 
+        --new mapping for completion(to cooperate the snippets)
         ["<Tab>"] = cmp.mapping(function(fallback)
-            if cmp.visible() then
-                cmp.select_next_item()
-            elseif luasnip.expand_or_jumpable() then
+            if luasnip.expand_or_jumpable() then
                 luasnip.expand_or_jump()
-            elseif has_words_before() then
-                cmp.complete()
             else
                 fallback()
             end
         end, { "i", "s" }),
         ["<S-Tab>"] = cmp.mapping(function(fallback)
-            if cmp.visible() then
-                cmp.select_prev_item()
-            elseif luasnip.jumpable(-1) then
+            if luasnip.jumpable(-1) then
                 luasnip.jump(-1)
             else
                 fallback()
