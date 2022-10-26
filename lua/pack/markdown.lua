5d6efeb99c575209013ec29c96c136a46cd05361
aklk1ng <2628084745@qq.com>
2022-10-26 11:03:20 +0800

    change the markdown browser

--- lua/pack/markdown.lua
+++ lua/pack/markdown.lua
@@ -6,7 +6,7 @@ function M.config()
     G.g.mkdp_markdown_css = '~/.config/nvim/colors/markdown.css'
     G.g.mkdp_page_title = '${name}'
     G.g.mkdp_preview_options = { hide_yaml_meta = 1, disable_filename = 1 }
-    G.g.mkdp_browserfunc = ''
+    G.g.mkdp_browser= 'surf'
 end
 
 function M.setup()
