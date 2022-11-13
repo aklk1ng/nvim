d534f603f1dabee8f0120eb10478f75ef04cdb66
aklk1ng <2628084745@qq.com>
2022-11-13 17:17:27 +0800

    add markdown's comment

--- lua/pack/markdown.lua
+++ lua/pack/markdown.lua
@@ -6,6 +6,7 @@ function M.config()
     G.g.mkdp_markdown_css = '~/.config/nvim/colors/markdown.css'
     G.g.mkdp_page_title = '${name}'
     G.g.mkdp_preview_options = { hide_yaml_meta = 1, disable_filename = 1 }
+    -- i only use next config in my Archlinux(if you don't have the surf browser, comment the next line or move it)
     G.g.mkdp_browser= 'surf'
 end
 
