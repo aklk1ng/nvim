da2fdb4608cebafbf179f01be92d4e091527fa8c
aklk1ng <2628084745@qq.com>
2022-11-23 17:06:55 +0800

    del the sql plugin configf

--- lua/profile.lua
+++ lua/profile.lua
@@ -26,21 +26,6 @@ vim.o.autoindent = true
 vim.o.smartindent = true
 vim.o.number = true
 
-vim.cmd([[
-    " hrsh7th/nvim-cmp
-    autocmd FileType sql,mysql,plsql lua require('cmp').setup.buffer({ sources = {{ name = 'vim-dadbod-completion' }} })
-    " Source is automatically added, you just need to include it in the chain complete list
-    let g:completion_chain_complete_list = {
-	   \   'sql': [
-	   \    {'complete_items': ['vim-dadbod-completion']},
-	   \   ],
-	   \ }
-    " Make sure `substring` is part of this list. Other items are optional for this completion source
-    let g:completion_matching_strategy_list = ['exact', 'substring']
-    " Useful if there's a lot of camel case items
-    let g:completion_matching_ignore_case = 1
-]])
-
 --leap
 require('leap').add_default_mappings()
 --highlight the search result
