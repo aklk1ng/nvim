6e2d4d9d1116b180239af52a2b47a1906700408d
aklk1ng <2628084745@qq.com>
2022-11-30 17:39:52 +0800

    udpate

--- lua/packer-init.lua
+++ lua/packer-init.lua
@@ -112,7 +112,7 @@ require('packer').startup({
         use { "windwp/nvim-ts-autotag", after = { 'nvim-treesitter' } }
         use { "windwp/nvim-autopairs"}
         --colorizer(highlight the color)
-        use { 'NvChad/nvim-colorizer.lua', event = 'BufRead', config = "require('pack.colorizer')"}
+        use { 'NvChad/nvim-colorizer.lua', config = "require('pack.colorizer')"}
 
         -------------------- highlight the current word
         use { 'itchyny/vim-cursorword', event = 'BufRead'}
