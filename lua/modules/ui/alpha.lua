f5fe5efcafe795e89d5b0356d5de51db49116cc0
aklk1ng <2628084745@qq.com>
2022-12-10 16:15:58 +0800

    change the dashboard theme

--- lua/modules/ui/alpha.lua
+++ lua/modules/ui/alpha.lua
@@ -1,147 +1,52 @@
 local M = {}
 function M.setup()
-    local status_ok, alpha = pcall(require, 'alpha')
-    if not status_ok then
-        vim.notify("alpha not found")
-        return
-    end
+local alpha = require("alpha")
+local dashboard = require("alpha.themes.dashboard")
 
-    local startify = require('alpha.themes.startify')
-    local fortune = require('alpha.fortune')
+-- Set header
+dashboard.section.header.val = {
+    "                                                     ",
+    "  ███╗   ██╗███████╗ ██████╗ ██╗   ██╗██╗███╗   ███╗ ",
+    "  ████╗  ██║██╔════╝██╔═══██╗██║   ██║██║████╗ ████║ ",
+    "  ██╔██╗ ██║█████╗  ██║   ██║██║   ██║██║██╔████╔██║ ",
+    "  ██║╚██╗██║██╔══╝  ██║   ██║╚██╗ ██╔╝██║██║╚██╔╝██║ ",
+    "  ██║ ╚████║███████╗╚██████╔╝ ╚████╔╝ ██║██║ ╚═╝ ██║ ",
+    "  ╚═╝  ╚═══╝╚══════╝ ╚═════╝   ╚═══╝  ╚═╝╚═╝     ╚═╝ ",
+    "                                                     ",
+}
 
-    local logo = {
-        type = 'text',
-        val = {
-            '                                                    ',
-            ' ███╗   ██╗███████╗ ██████╗ ██╗   ██╗██╗███╗   ███╗ ',
-            ' ████╗  ██║██╔════╝██╔═══██╗██║   ██║██║████╗ ████║ ',
-            ' ██╔██╗ ██║█████╗  ██║   ██║██║   ██║██║██╔████╔██║ ',
-            ' ██║╚██╗██║██╔══╝  ██║   ██║╚██╗ ██╔╝██║██║╚██╔╝██║ ',
-            ' ██║ ╚████║███████╗╚██████╔╝ ╚████╔╝ ██║██║ ╚═╝ ██║ ',
-            ' ╚═╝  ╚═══╝╚══════╝ ╚═════╝   ╚═══╝  ╚═╝╚═╝     ╚═╝ ',
-        },
-        opts = {
-            position = 'left',
-            hl = 'DevIconVim',
-        },
-    }
+-- Set menu
+dashboard.section.buttons.val = {
+    dashboard.button( "e", "  > New file" , ":ene <BAR> startinsert <CR>"),
+    dashboard.button( "ff", "  > Find file", ":FzfLua files<CR>"),
+    dashboard.button( "fo", "  > Recent"   , ":FzfLua oldfiles<CR>"),
+    dashboard.button( "s", "  > Settings" , ":e $MYVIMRC | :cd %:p:h | split . | wincmd k | pwd<CR>"),
+    dashboard.button( "u", "  > Update" , ":PackerSync"),
+    dashboard.button( "q", "  > Quit NVIM", ":qa<CR>"),
+}
 
-    local function info_value()
-        local total_plugins = #vim.tbl_keys(packer_plugins)
-        local datetime = os.date(' %d-%m-%Y')
-        local version = vim.version()
-        local nvim_version_info = '   v' .. version.major .. '.' .. version.minor .. '.' .. version.patch
+-- Set footer
+--   NOTE: This is currently a feature in my fork of alpha-nvim (opened PR #21, will update snippet if added to main)
+--   To see test this yourself, add the function as a dependecy in packer and uncomment the footer lines
+--   ```init.lua
+--   return require('packer').startup(function()
+--       use 'wbthomason/packer.nvim'
+--       use {
+--           'goolord/alpha-nvim', branch = 'feature/startify-fortune',
+--           requires = {'BlakeJC94/alpha-nvim-fortune'},
+--           config = function() require("config.alpha") end
+--       }
+--   end)
+--   ```
+-- local fortune = require("alpha.fortune") 
+-- dashboard.section.footer.val = fortune()
 
-        return '        ' .. datetime .. '   ' .. total_plugins .. ' plugins' .. nvim_version_info
-    end
+-- Send config to alpha
+alpha.setup(dashboard.opts)
 
-    local info = {
-        type = 'text',
-        val = info_value(),
-        opts = {
-            hl = 'DevIconVim',
-            position = 'left',
-        },
-    }
-
-    local message = {
-        type = 'text',
-        val = fortune({ max_width = 60 }),
-        opts = {
-            position = 'left',
-            hl = 'Statement',
-        },
-    }
-
-    local header = {
-        type = 'group',
-        val = {
-            logo,
-            info,
-            message,
-        },
-    }
-
-    local mru = {
-        type = 'group',
-        val = {
-            {
-                type = 'text',
-                val = 'Recent files',
-                opts = {
-                    hl = 'String',
-                    shrink_margin = false,
-                    position = 'left',
-                },
-            },
-            { type = 'padding', val = 1 },
-            {
-                type = 'group',
-                val = function()
-                    return { startify.mru(1, vim.fn.getcwd(), 22) }
-                end,
-            },
-        },
-    }
-
-    local buttons = {
-        type = 'group',
-        val = {
-            {
-                type = 'text',
-                val = 'Actions',
-                opts = {
-                    hl = 'String',
-                    shrink_margin = false,
-                    position = 'left',
-                },
-            },
-            { type = 'padding', val = 1 },
-            startify.button('e', '  New file', ':ene <BAR> startinsert<CR>'),
-            -- if i use next commands i can't open the FzfLua normally(other FzfLua commands are normal)
-            -- startify.button('ff', '  Find file', "<cmd>lua require('fzf-lua').files()<CR>"),
-            -- startify.button('fo', '  Old files', "<cmd>lua require('fzf-lua').oldfiles()<CR>"),
-            startify.button('u', '  Update plugins', ':PackerSync<CR>'),
-            startify.button('q', '  Quit', ':qa<CR>'),
-        },
-        opts = {
-            position = 'left',
-        },
-    }
-
-    local config = {
-        layout = {
-            header,
-            { type = 'padding', val = 1 },
-            mru,
-            { type = 'padding', val = 1 },
-            buttons,
-        },
-        opts = {
-            setup = function()
-                vim.api.nvim_create_autocmd('User', {
-                    pattern = 'AlphaReady',
-                    desc = 'disable status, tabline and cmdline for alpha',
-                    callback = function()
-                        vim.go.laststatus = 0
-                        vim.opt.showtabline = 0
-                        vim.opt.cmdheight = 0
-                    end,
-                })
-                vim.api.nvim_create_autocmd('BufUnload', {
-                    buffer = 0,
-                    desc = 'enable status, tabline and cmdline after alpha',
-                    callback = function()
-                        vim.go.laststatus = 3
-                        vim.opt.showtabline = 2
-                        vim.opt.cmdheight = 1
-                    end,
-                })
-            end,
-            margin = 5,
-        },
-    }
-
-    alpha.setup(config)
+-- Disable folding on alpha buffer
+vim.cmd([[
+    autocmd FileType alpha setlocal nofoldenable
+]])
 end
 return M
